4e6bcc807f705c4f56a1d7f1d1316575bb149f39
preyxnshhhh <preyxnshhh@gmail.com>
2026-09-02 17:34:55 +0530

    Add files via upload

---
+++ Emp Salary.py
@@ -0,0 +1,13 @@
+n=input("Enter the name of employee: ")
+b=float(input("Enter basic salary: "))
+hra=0.2*b
+da=0.1*b
+pf=0.12*b
+g=b+hra+da
+n=g-pf
+print(hra)
+print(da)
+print(pf)
+print(g)
+print(n)
+
